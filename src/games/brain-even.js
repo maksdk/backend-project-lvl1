64e704c196aae76b83ad619642cfca328046ab64
maksdk <havryshchenko@gmail.com>
2020-02-20 08:53:10 +0200

    added random for even game

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -4,11 +4,24 @@
 import game from "../index.js";
 import colors from 'colors';
 
+const randomNumber = (min, max) => {
+    return Math.floor(min + Math.random() * (max - min + 1));
+};
+
+const isEven = num => num % 2 === 0;
+
+const buildRounds = () => {
+	const rounds = [];
+	for(let i = 0; i < 3; i++) {
+		const number = randomNumber(0, 20);
+		const question = String(number);
+		const answer = isEven(number) ? "yes" : "no";
+		rounds.push({ question, answer });
+	}
+	return rounds;
+};
+
 const task = `Answer ${colors.red('"yes"')} ${colors.bold("if")} the number is even, otherwise answer ${colors.red('"no"')}${colors.blue(".")}`;
-const rounds = [
-    { question: '15', answer: 'no' },
-    { question: '6', answer: 'yes' },
-    { question: '7', answer: 'no' }
-];
+const rounds = buildRounds();
 
 export default () => game({ rounds, task });
