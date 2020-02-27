dc66e1dcbbe0cbb4506b2fe8cdbd9dbc2845c6d0
maksdk <havryshchenko@gmail.com>
2020-02-27 22:00:29 +0200

    remove prime checking

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -7,16 +7,6 @@ const MIN_NUMBER = 1;
 const MAX_NUMBER = 20;
 const ROUNDS_COUNT = 3;
 
-const isPrime = (num) => {
-  if (num < 2) return false;
-
-  for (let i = 2; i <= num / 2; i += 1) {
-    if (num % i === 0) return false;
-  }
-
-  return true;
-};
-
 const getGreatestCommonDivisor = (num1, num2) => {
   let n1 = Math.abs(num1);
   let n2 = Math.abs(num2);
@@ -34,11 +24,8 @@ const createRounds = () => {
   const rounds = [];
 
   for (let i = 0; i < ROUNDS_COUNT; i += 1) {
-    let number1 = Utils.randomNumber(MIN_NUMBER, MAX_NUMBER);
-    number1 = Utils.isPrime(number1) ? number1 + 1 : number1;
-
-    let number2 = Utils.randomNumber(MIN_NUMBER, MAX_NUMBER);
-    number2 = isPrime(number2) ? number2 + 1 : number2;
+    const number1 = Utils.randomNumber(MIN_NUMBER, MAX_NUMBER);
+    const number2 = Utils.randomNumber(MIN_NUMBER, MAX_NUMBER);
 
     const question = `${number1} ${number2}`;
     const answer = String(getGreatestCommonDivisor(number1, number2));
