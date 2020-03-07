fbcd264c56aac8035bf19e532417492bf19c850a
maksdk <havryshchenko@gmail.com>
2020-03-07 14:01:21 +0200

    remade algoritm of round generating

--- src/games/even.js
+++ src/games/even.js
@@ -6,27 +6,20 @@ import { randomNumber } from '../utils/index.js';
 
 const minNumber = 0;
 const maxNumber = 20;
-const roundsCount = 3;
 
 const correctAnswer = 'yes';
 const wrongAnswer = 'no';
 
 const isEven = (num) => num % 2 === 0;
 
-const createRounds = () => {
-  const rounds = [];
+const generateRound = () => {
+  const number = randomNumber(minNumber, maxNumber);
+  const question = String(number);
+  const answer = isEven(number) ? correctAnswer : wrongAnswer;
 
-  for (let i = 0; i < roundsCount; i += 1) {
-    const number = randomNumber(minNumber, maxNumber);
-    const question = String(number);
-    const answer = isEven(number) ? correctAnswer : wrongAnswer;
-
-    rounds.push({ question, answer });
-  }
-
-  return rounds;
+  return { question, answer };
 };
 
 const task = `Answer ${colors.red(`"${correctAnswer}"`)} ${colors.bold('if')} the number is even, otherwise answer ${colors.red(`"${wrongAnswer}"`)}.`;
 
-export default () => play(task, createRounds);
+export default () => play(task, generateRound);
