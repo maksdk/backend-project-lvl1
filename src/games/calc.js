f70729251f8cb479551876d270a2ddd49f68bc6f
maksdk <havryshchenko@gmail.com>
2020-03-07 13:57:56 +0200

    remade algoritm of round generating

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,7 +5,6 @@ import { randomNumber } from '../utils/index.js';
 
 const minNumber = 0;
 const maxNumber = 10;
-const roundsCount = 3;
 
 const operations = {
   '*': (num1, num2) => num1 * num2,
@@ -13,26 +12,21 @@ const operations = {
   '-': (num1, num2) => num1 - num2,
 };
 
-const createRounds = () => {
+const generateRound = () => {
   const operationsNames = Object.keys(operations);
-  const rounds = [];
 
-  for (let i = 0; i < roundsCount; i += 1) {
-    const num1 = randomNumber(minNumber, maxNumber);
-    const num2 = randomNumber(minNumber, maxNumber);
+  const num1 = randomNumber(minNumber, maxNumber);
+  const num2 = randomNumber(minNumber, maxNumber);
 
-    const operationName = operationsNames[randomNumber(0, operationsNames.length - 1)];
-    const operation = operations[operationName];
+  const operationName = operationsNames[randomNumber(0, operationsNames.length - 1)];
+  const operation = operations[operationName];
 
-    const question = `${num1} ${operationName} ${num2}`;
-    const answer = String(operation(num1, num2));
+  const question = `${num1} ${operationName} ${num2}`;
+  const answer = String(operation(num1, num2));
 
-    rounds.push({ answer, question });
-  }
-
-  return rounds;
+  return { question, answer };
 };
 
 const task = 'What is the result of the expression?';
 
-export default () => play(task, createRounds);
+export default () => play(task, generateRound);
